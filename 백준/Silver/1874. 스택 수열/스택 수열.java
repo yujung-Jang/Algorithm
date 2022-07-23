52e6312c880225179248643c7c3397be2237f572
Yujung Jang <yjung.dev@gmail.com>
2022-07-24 08:54:03 +0900

    [Silver III] Title: 스택 수열, Time: 1004 ms, Memory: 113492 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int last = 0;
+
+        while (n --> 0) {
+            int input = sc.nextInt();
+
+            //입력된 값이 현재의 마지막 값보다 클 때
+            if (input > last) {
+                
+                for (int j = last + 1; j <= input; j++) {
+                    stack.push(j);
+                    sb.append('+').append('\n');
+                }
+                last = input;
+            }
+
+            //top의 원소가 입력받은 수와 같지 않을 때
+            else if (stack.peek() != input) {
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append('-').append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
