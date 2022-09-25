33468705d9959e9d61c740956503a50f7370e650
Yujung Jang <yjung.dev@gmail.com>
2022-09-25 21:04:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 27.79 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,19 +3,23 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
 
-        ArrayList<Integer> list = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
 
+        //배열의 요소들을 이전 요소와 비교해서 같지 않다면 Queue에 넣는다
         int pre = 10;
-        for (int i : arr) {
-            if (i != pre) {
-                list.add(i);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != pre) {
+                q.add(arr[i]);
+                pre = arr[i];
             }
-            pre = i;
         }
 
-        int[] result = new int[list.size()];
+        //중복이 제거된 Queue의 크기와 같은 배열을 생성한다
+        int[] result = new int[q.size()];
+
+        //생성한 배열에 Queue의 요소를 꺼내서 넣는다
         for (int i = 0; i < result.length; i++) {
-            result[i] = list.get(i);
+            result[i] = q.poll();
         }
 
         return result;
