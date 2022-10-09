aa3b6c4e8daca89acfe01f9d97019d2962d40c51
Yujung Jang <yjung.dev@gmail.com>
2022-10-09 20:54:57 +0900

    [level 2] Title: 전화번호 목록, Time: 221.71 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -10,10 +10,9 @@ class Solution {
             map.put(phone_book[i], i);
 
 
-        // 만약 각 Key를 포함하는 다른 Key가 존재한다면 count에 기록한다
         for (int i = 0; i < phone_book.length; i++) {
             // 각 번호의 여러 일부에 해당하는 key가 있는지 찾는다
-            for (int j = 0; j < phone_book[i].length(); j++) { // 12 123
+            for (int j = 0; j < phone_book[i].length(); j++) {
                 if (map.containsKey(phone_book[i].substring(0, j)))
                     return false;
             }
