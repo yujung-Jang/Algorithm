bc31829008d0301dc690dcd4124f23a40fb4813c
Yujung Jang <yjung.dev@gmail.com>
2023-01-29 14:15:20 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,5 +1,5 @@
 def solution(n):
-    if (n ** 0.5) == int((n ** 0.5)) and (n ** 0.5) ** 2:
+    if (n ** 0.5) == int((n ** 0.5)):
         return ((n ** 0.5) + 1) ** 2
     else:
         return -1
