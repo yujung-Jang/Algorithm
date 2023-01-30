df39b85fe603a0484a3ecd9c7332ecd333969673
Yujung Jang <yjung.dev@gmail.com>
2023-01-30 19:32:43 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/README.md
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 나누어 떨어지는 숫자 배열 - 12910 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12910) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=python3) 
 
 ### 성능 요약
 
-메모리: 7.66 MB, 시간: 0.03 ms
+메모리: 10.3 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -1,13 +1,6 @@
 def solution(arr, divisor):
-    
     answer = []
     for i in arr:
         if i % divisor == 0:
             answer.append(i)
-    
-    answer.sort()
-    
-    if len(answer) == 0:
-        answer.append(-1)
-    
-    return answer
+    return sorted(answer) if len(answer) != 0 else [-1]
