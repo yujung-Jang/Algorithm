d4a56f6815be0391f1083ae2194b60b6eca09a12
Yujung Jang <yjung.dev@gmail.com>
2023-01-30 14:55:42 +0900

    [level 1] Title: 최소직사각형, Time: 2.93 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,15 +1,2 @@
 def solution(sizes):
-    for i in sizes:
-        if i[1] < i[0]:
-            temp = i[0]
-            i[0] = i[1]
-            i[1] = temp
-            
-    w = 0
-    h = 0
-    for i in sizes:
-        if i[0] > w:
-            w = i[0]
-        if i[1] > h:
-            h = i[1]
-    return w * h
+    return max(max(i) for i in sizes) * max(min(i) for i in sizes)
