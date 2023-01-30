f1e7a98b6dc7cf966f8a61c1ff4dbaba5c81b960
Yujung Jang <yjung.dev@gmail.com>
2023-01-30 19:27:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -4,16 +4,13 @@ def solution(num):
         return 0
     
     answer = 0
-    
     while num != 1:
-        answer += 1
-        
-        if num % 2 == 0:
-            num = num // 2
+        if answer >= 500:
+            return -1
+        elif num % 2 == 0:
+            num //= 2
+            answer += 1
         elif num % 2 == 1:
             num = num * 3 + 1
-        
-        if answer == 500:
-            return -1
-    
+            answer += 1
     return answer
