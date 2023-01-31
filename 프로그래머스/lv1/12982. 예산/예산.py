7c896f0a655e77ae7594d5473e9f4e3d926a1120
Yujung Jang <yjung.dev@gmail.com>
2023-01-31 20:41:37 +0900

    [level 1] Title: 예산, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.py
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -1,7 +1,5 @@
 def solution(d, budget):
     d.sort()
-    arr = []
-    for i in range(len(d)):
-        if sum(arr) + d[i] <= budget:
-            arr.append(d[i])
-    return len(arr)
+    while sum(d) > budget:
+        d.pop()
+    return len(d)
