b866a8f2b2187cf2e117f2d360a46cf1f6661920
Yujung Jang <yjung.dev@gmail.com>
2023-01-31 09:19:59 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,12 +1,2 @@
 def solution(price, money, count):
-    
-    N = 1
-    _sum = 0
-    for i in range(1, count + 1):
-        _sum += price * N
-        N += 1
-    
-    if _sum > money:
-        return _sum - money
-    else:
-        return 0
+    return max(0, (price * count * (count + 1) // 2) - money) # 산술 평균
