76c830f90989090026a9ee28392e2191c6b0deb7
Yujung Jang <yjung.dev@gmail.com>
2023-01-31 20:38:11 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,13 +1,9 @@
 def solution(s):
     
-    answer = ""
-    sr = s.split(" ")
-    for i in sr:
-        for j in range(len(i)):
-            if j % 2 == 0:
-                answer += i[j].upper()
-            else:
-                answer += i[j].lower()
-        answer += " "
-    return answer[0:-1]
+    '''
+    1. s를 공백을 기준으로 나눈 리스트 A를 만든다. (split())
+    2. map을 통해 A의 각 요소에 접근하여 lambda 함수를 적용한다. (대소문자 전환)
+    3. 리스트 A를 공백으로 구분하여 하나의 문자로 만든다. (join())
+    '''
+    return ' '.join(map(lambda x: ''.join([n.lower() if i % 2 else n.upper() for i, n in enumerate(x)]), s.split(' ')))
             
